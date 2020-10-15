e0e77df5e46330efd6ded54ade5b02bb2ef0dd84
Tim Hunter <thorrsson@github.com>
2020-10-15 11:01:08 -0700

    update to use master

--- functions.js
+++ functions.js
@@ -7,9 +7,9 @@ function createMapping(array) {
       user: review.user.login,
       state: review.state,
     }))
-    .filter(review => review.state === 'APPROVED')
+    // .filter(review => review.state === 'APPROVED')
     // Now we have an array like: [ { number: 123, user: 'DavideViolante', state: 'APPROVED' }, ... ]
-    .reduce((obj, review) => (obj[review.number] = (obj[review.number] || 0) + 1, obj), {})
+    .reduce((obj, review) => (obj[review.number] = 1, obj), {})
     // Now we have an object like: { '123': 3, '456': 1, ... }
 }
 
